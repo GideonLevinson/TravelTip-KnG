92960940e818330853675ff928a1df1968bfdcf4
GideonLevinson <gideonlevinson@gmail.com>
2023-05-03 22:23:26 +0300

    add getLoc,addLoc

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -6,7 +6,7 @@ const LOC_KEY = 'locDB'
 export const locService = {
     getLocs,
     addLoc,
-    deletLoc,
+    deleteLoc,
 }
 
 // onAddMarker-> nothing in loc.service
@@ -23,7 +23,6 @@ const locs = [
     { id: 'AaBb1', name: 'Greatplace', lat: 32.047104, lng: 34.832384 },
     { id: 'AaBb2', name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
 ]
-console.log('locs:', locs)
 
 function getLocs() {
     return new Promise((resolve, reject) => {
@@ -33,14 +32,39 @@ function getLocs() {
     })
 }
 
+function getLoc(term) {
+    console.log('Getting from Network')
+    term = term.replaceAll(' ', '+')
+    const API_KEY = 'AIzaSyCoOL7doqQx-Oz3NrSRgbw4yPpsMBvcAY8'
+    return axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${term}&key=${API_KEY}`)
+        .then(res => res.data.results[0])
+        .then(loc => ({
+            name: loc.formatted_address,
+            pos: loc.geometry.location
+        }))
+        .then(loc => {
+            addLoc(loc)
+        })
+}
+
 function get(locId) {
-    return storageService.get(PET_KEY, petId)
+    return storageService.get(LOC_KEY, locId)
 }
 
-function addLoc() {
+function addLoc(loc) {
     console.log('adding...')
+    const newLocId = utilService.makeId()
+    loc['id'] = newLocId
+    locs.push(loc)
 }
 
-function deletLoc() {
+function deleteLoc() {
     console.log('deleting...')
+}
+
+function getLocIdxById(locId) {
+    getLocs()
+        .then(res => {
+            return locs.findIndex(loc => loc.id === locId)
+        })
 }
