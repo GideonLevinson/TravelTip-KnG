75ab26c4f801d40ff3efe289aafe06ab3981a094
GideonLevinson <gideonlevinson@gmail.com>
2023-05-03 18:09:03 +0300

    add delete and add functions

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,12 +1,29 @@
+import { utilService } from './util.service.js'
+import { storageService } from './async-storage.service.js'
+
+const LOC_KEY = 'locDB'
+
 export const locService = {
-    getLocs
+    getLocs,
+    addLoc,
+    deletLoc,
 }
 
+// onAddMarker
+// onAddLoc addLoc
+// onDeleteLocation
+// onGoToLocation
+// onCopyLocation
+// onSearch
 
+// Demo data, in 'pets' we had '_creatPets' to do it
+
+// I want to add id, weather, createdAt, updatedAt 
 const locs = [
-    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 }, 
-    { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
+    { id:AaBb1, name: 'Greatplace', lat: 32.047104, lng: 34.832384, wheather, createdAt, updatedAt }, 
+    { id:AaBb2, name: 'Neveragain', lat: 32.047201, lng: 34.832581, wheather, createdAt, updatedAt }
 ]
+console.log('locs:', locs)
 
 function getLocs() {
     return new Promise((resolve, reject) => {
@@ -16,4 +33,14 @@ function getLocs() {
     })
 }
 
+function get(locId) {
+    return storageService.get(PET_KEY, petId)
+}
 
+function addLoc() {
+    console.log('adding...')
+}
+
+function deletLoc(){
+    console.log('deleting...')
+}
